6f93f46f008d1de58e41108b9458756e423f78bd
Yasin Gole <yasingole98@gmail.com>
2023-09-30 19:26:05 +0100

    .

--- Portfolio_website/infrastructure/cloudfront.tf
+++ Portfolio_website/infrastructure/cloudfront.tf
@@ -73,3 +73,7 @@ resource "aws_cloudfront_distribution" "web_distribution" {
 
   depends_on = [ aws_acm_certificate.certificate, aws_route53_record.certificate_validation ]
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.web_distribution.id
+}
