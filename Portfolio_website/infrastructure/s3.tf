d9c9913ac6d1b08e66473197c88389d72d7916cb
Yasin Gole <yasingole98@gmail.com>
2023-10-02 20:00:39 +0100

    .

--- Portfolio_website/infrastructure/s3.tf
+++ Portfolio_website/infrastructure/s3.tf
@@ -19,11 +19,11 @@ resource "aws_s3_bucket_website_configuration" "web_bucket_config" {
   bucket = aws_s3_bucket.web_bucket.id
 
   index_document {
-    suffix = "app.js"
+    suffix = "index.html"
   }
   
   error_document {
-    key = "app.js"
+    key = "index.html"
   }
 }
 
