e3bf1b3e78334d15627f40cb52dc0ade44af44fe
Yasin Gole <yasingole98@gmail.com>
2023-10-04 19:30:33 +0100

    .

--- Portfolio_website/infrastructure/lambda_counter.tf
+++ Portfolio_website/infrastructure/lambda_counter.tf
@@ -1,55 +1,55 @@
+# Fetch the AWS account ID dynamically
+data "aws_caller_identity" "current" {}
+
 # Creating an IAM role for the Lambda function
 resource "aws_iam_role" "increment_visitor_count_role" {
   name = "increment_visitor_count"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+      }
+    ]
+  })
 }
 
 # Creating a policy for the role to access DynamoDB
 resource "aws_iam_policy" "increment_visitor_count_policy" {
   name        = "increment_visitor_count_policy"
   description = "Allow Lambda to access DynamoDB"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "dynamodb:UpdateItem"
-      ],
-      "Resource": [
-        "arn:aws:dynamodb:us-east-1:YOUR_ACCOUNT_ID:table/cloud-resume-stats"
-      ]
-    }
-  ]
-}
-EOF
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:UpdateItem"
+        ],
+        Resource = [
+          "arn:aws:dynamodb:us-east-1:${data.aws_caller_identity.current.account_id}:table/cloud-resume-stats"
+        ]
+      }
+    ]
+  })
 }
 
 # Attachment of policy to role
-resource "aws_iam_role_policy_attachment" "increment_visitor_count_attachment" {
-  role       = aws_iam_role.increment_visitor_count_role.name
-  policy_arn = aws_iam_policy.increment_visitor_count_policy.arn
+resource "aws_iam_policy_attachment" "increment_visitor_count_attachment" {
+  name        = "attach-increment-visitor-count-policy"  # Provide a name for the attachment
+  policy_arn  = aws_iam_policy.increment_visitor_count_policy.arn
+  roles       = [aws_iam_role.increment_visitor_count_role.name]
 }
+
 
 # Lambda function code to increment visitor count
 data "archive_file" "increment_visitor_count_lambda_zip" {
   type        = "zip"
-  source_dir  = "visitor_count.py"  # Adjust the source directory here
+  source_file  = "visitor_count.py"  # Adjust the source file here
   output_path = "visitor_count_lambda.zip"
 }
 
